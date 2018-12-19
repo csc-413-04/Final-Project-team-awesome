e700ea135dde01e6485d491f15597d8c4fffe425
merewanj <mehe_jesa@yahoo.com>
2018-12-18 20:10:23 -0800

    renewing main

--- homework2/src/main/java/Main.java
+++ homework2/src/main/java/Main.java
@@ -24,3 +24,62 @@ public class Main {
     }
 
 
+
+
+    public static void main(String[] args) {
+        MongoClient mongoClient = new MongoClient("localhost", 27017);
+        MongoDatabase db = mongoClient.getDatabase("REST2");
+        WriteData data_writer = new WriteData();
+
+        //Creating Collections
+        MongoCollection<Document> userCollect = db.getCollection("users");
+        MongoCollection<Document> authCollect = db.getCollection("auth");
+        MongoCollection<Document> postCollect = db.getCollection("post");
+        MongoCollection<Document> commentCollect = db.getCollection("comment");
+        // staticFiles.externalLocation("public");
+        // http://sparkjava.com/documentation
+        port(4321);
+
+        //Map<String, Object> map = new HashMap<String, Object>();
+        //BasicBSONList map = new BasicBSONList();
+        ArrayList<Object> map = new ArrayList<Object>();
+
+        // calling get will make your app start listening for the GET path with the /hello endpoint
+        get("/hello", (req, res) -> "<h1 style='font-family:sans-serif'>Welcome to Spark REST!!!</h1><br/>");
+
+        //newuser
+        //get("/newuser", (req, res) -> "okay");
+        get("/newuser", (req, res)-> {
+            //Getting the username value
+            int count = 0;
+            String output = "";
+            String username = req.queryParams("username");
+            System.out.print(username);
+            //Getting the pasword value
+            String password = req.queryParams("password");
+            String email = req.queryParams("email");
+            Document document = new Document("username", username);
+            Document dummy = new Document();
+            //If there are no documents inside the collection yet...
+            if (userCollect.count() == 0) {
+                document.append("username", username).append("password", password).append("id", AutoID());
+            }
+            // if that's not the case then...
+            else {
+                // retrieve the last documents to get the latest id
+                MongoIterable<Document> doc = userCollect.find();
+                MongoCursor cursor = doc.iterator();
+                // while there are still documents...
+                while (cursor.hasNext()) {
+                    dummy = (Document) cursor.next();
+                    if ((int) dummy.get("id") > count) {
+                        count = (int) dummy.get("id");
+                    }
+                }
+                document.append("username", username).append("password", password).append("email", email).append("id", count + 1);
+            }
+            document.append("friends", map);
+            MongoIterable<Document> findUser = userCollect.find();
+            MongoCursor<Document> userCursor = findUser.iterator();
+
+
