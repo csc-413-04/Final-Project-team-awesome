aecc1ce9d97e2e84d7073ac74bb08e66797be041
acheron7 <dadams03@gmail.com>
2018-12-18 20:58:28 -0800

    imports and post class

---
+++ homework2/c_blog/src/Component/post/my_post.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import NavBar from '../navbar';
+import TextData from '../../dataText.json';
+import PostData from '../../post.json';
+import Popup from "reactjs-popup";
+import SweetAlert from "react-bootstrap-sweetalert";
+import { check } from '../navbar_action/checkUser';
+import ReactDOM from 'react-dom'
+
+var values;
+
+class Post extends Component {}
