edc532e168900dd54ecf2c436207e9d785a3e485
Abdulaziz Alharbi <abdulazizalharbi@sfs-wifi-dhcp-10-143-255-208.wifi.sfsu.edu>
2018-12-18 21:58:08 -0800

    Adding render

--- homework2/c_blog/src/Component/post/post.js
+++ homework2/c_blog/src/Component/post/post.js
@@ -4,3 +4,26 @@ import TextData from '../../dataText.json';
 import NavBar from '../../Component/navbar';
 
 class Post extends Component {
+	
+	 constructor(props) {
+        super();
+        this.state = {
+          username: TextData.username,
+          link: "http://localhost:4321/create_post",
+          posts: PostData.posts,
+          trueorfalse: true
+        }
+	
+	
+}
+
+   render(){
+
+        return(
+            <div>
+            <NavBar />
+            
+
+            <div class="container">
+            <table class="table">
+            <thead>
