9accf1b21961e9e425e90a294e8db5f599740af8
Abdulaziz Alharbi <abdulazizalharbi@sfs-wifi-dhcp-10-143-255-208.wifi.sfsu.edu>
2018-12-18 21:54:12 -0800

    adding class post extends components

--- homework2/c_blog/src/Component/post/post.js
+++ homework2/c_blog/src/Component/post/post.js
@@ -2,3 +2,5 @@ import React, {Component} from 'react';
 import PostData from '../../post.json';
 import TextData from '../../dataText.json';
 import NavBar from '../../Component/navbar';
+
+class Post extends Component {
