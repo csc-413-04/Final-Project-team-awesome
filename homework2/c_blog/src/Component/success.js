b98e541b76c4d3829846585ba0f30286b5908293
tahirdar <tahirdar@mail.sfsu.edu>
2018-12-20 18:52:46 -0800

    Update success.js

--- homework2/c_blog/src/Component/success.js
+++ homework2/c_blog/src/Component/success.js
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
 import NavBar from './navbar';
+import FooterReact from './footer';
 import { MDBContainer, MDBRow, MDBCol, MDBInput, MDBBtn, MDBListGroup, MDBListGroupItem, Col, Container, Row } from 'mdbreact';
 class Success extends Component {
     render () {
+        
         return (
             <div>
                 <NavBar />
                 <h1>Success!!!</h1>
+            <FooterReact/>
             </div>
         );
     }
