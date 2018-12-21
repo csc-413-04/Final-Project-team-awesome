85a01099b7c08045ec143490fd2169cbe230e629
loucid <syd.morano@gmail.com>
2018-12-20 17:55:57 -0800

    changes

--- homework2/c_blog/src/Component/login/loginform.js
+++ homework2/c_blog/src/Component/login/loginform.js
@@ -2,5 +2,17 @@ import React, {Component} from 'react';
 import NavBar from '../navbar';
 class LoginForm extends Component {
 
+    constructor(props){
+        super(props);
+    
+        this.state = {
+            username: '',
+            password: '',
+            
+        
+        
+        }
+     }
 
+}
 export default LoginForm;
