bc3926c7f1ae63b28f5d5c3b5bc08e1e5de53954
acheron7 <dadams03@gmail.com>
2018-12-20 17:36:44 -0800

    added MDB

--- homework2/c_blog/src/Component/success.js
+++ homework2/c_blog/src/Component/success.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import NavBar from './navbar';
+import { MDBContainer, MDBRow, MDBCol, MDBInput, MDBBtn, MDBListGroup, MDBListGroupItem, Col, Container, Row } from 'mdbreact';
 class Success extends Component {
     render () {
         return (
