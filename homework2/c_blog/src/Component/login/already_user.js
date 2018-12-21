3fc1dd16517035b715e407df006abc33fab6ba86
acheron7 <dadams03@gmail.com>
2018-12-20 17:40:19 -0800

    added MDB import

--- homework2/c_blog/src/Component/login/already_user.js
+++ homework2/c_blog/src/Component/login/already_user.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import NavBar from '../navbar';
 import Already_Exists from '../already_exists';
+import { MDBContainer, MDBRow, MDBCol, MDBInput, MDBBtn, MDBListGroup, MDBListGroupItem, Col, Container, Row } from 'mdbreact';
+
 class AlreadyUser extends Component {
     render() {
         return (
