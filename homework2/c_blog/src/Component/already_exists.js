39c07ccd29c42718347e33af0f1a51bc9c8c8d09
tahirdar <tahirdar@mail.sfsu.edu>
2018-12-20 18:43:32 -0800

    Update already_exists.js

--- homework2/c_blog/src/Component/already_exists.js
+++ homework2/c_blog/src/Component/already_exists.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import NavBar from "./navbar";
+import FooterReact from './footer';
+
 import { MDBContainer, MDBRow, MDBCol, MDBInput, MDBBtn, MDBListGroup, MDBListGroupItem, Col, Container, Row } from 'mdbreact';
 class Already_Exists extends Component {
     render () {
