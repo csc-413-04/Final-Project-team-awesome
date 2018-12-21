8c9c67b33f4e096c76e9dc757b82b30029190a15
tahirdar <tahirdar@mail.sfsu.edu>
2018-12-20 18:49:37 -0800

    Update homepage.js

--- homework2/c_blog/src/Component/homepage.js
+++ homework2/c_blog/src/Component/homepage.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import NavBar from './navbar';
+import FooterReact from './footer';
 
 import { MDBContainer, MDBRow, MDBCol, MDBInput, MDBBtn, MDBListGroup, MDBListGroupItem, Col, Container, Row } from 'mdbreact';
 class HomePage extends Component {
@@ -24,7 +25,7 @@ class HomePage extends Component {
                         <br/><br/>
                         <input type="submit"  value="Submit" />
                     </form> 
-
+                <FooterReact/>
             </div>
         );
     }
